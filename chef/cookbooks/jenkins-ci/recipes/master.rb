e3471cd06643988ecdbbe94b9d8164703adae9ab
Oleksandr Semych <Oleksandr_Semych@epam.com>
2017-06-16 00:00:41 +0300

    plugins are now installed from file list

--- chef/cookbooks/jenkins-ci/recipes/master.rb
+++ chef/cookbooks/jenkins-ci/recipes/master.rb
@@ -8,6 +8,7 @@
 #
 
 include_recipe "jenkins::master"
+include_recipe "jenkins-ci::plugins"
 include_recipe 'maven'
 
 git_client 'default' do
@@ -26,82 +27,8 @@ git_config 'user.name' do
     user 'jenkins'
 end
 
-jenkins_plugin 'junit' do
-    version '1.2'
-    install_deps false
-end
-
-jenkins_plugin 'matrix-project' do
-    version '1.7.1'
-    install_deps false
-end
-
-jenkins_plugin 'scm-api' do
-    version '2.1.0'
-    install_deps false
-end
-
-jenkins_plugin 'credentials' do
-    version '2.1.13'
-    install_deps false
-end
-
-jenkins_plugin 'display-url-api' do
-    version '2.0'
-    install_deps false
-end
-
-jenkins_plugin 'token-macro' do
-    version '2.1'
-    install_deps false
-end
-
-jenkins_plugin 'workflow-scm-step' do
-    version '2.4'
-    install_deps false
-end
-
-jenkins_plugin 'workflow-step-api' do
-    version '2.9'
-    install_deps false
-end
-
-jenkins_plugin 'ssh-credentials' do
-    version '1.13'
-    install_deps false
-end
-
-jenkins_plugin 'plain-credentials' do
-    version '1.4'
-    install_deps false
-end
-
-jenkins_plugin 'git-client' do
-    version '2.4.5'
-    install_deps false
-end
-
-jenkins_plugin 'mailer' do
-    version '1.20'
-    install_deps false
-end
-
-jenkins_plugin 'script-security' do
-    version '1.27'
-    install_deps false
-end
-
-jenkins_plugin 'structs' do
-    version '1.6'
-    install_deps false
-end
-
-jenkins_plugin 'git' do
-    version '3.3.0'
-    install_deps false
-end
-
-jenkins_plugin 'job-dsl'
+secret = Chef::EncryptedDataBagItem.load_secret("/tmp/vagrant-chef/encrypted_data_bag_secret_key")
+credentials = Chef::EncryptedDataBagItem.load("jenkins", "ad_creds", secret)
 
 jenkins_script 'add_maven' do
   command <<-EOH.gsub(/^ {4}/, '')
@@ -118,6 +45,28 @@ jenkins_script 'add_maven' do
     )
     mavenTask.save()
   EOH
+end
+
+jenkins_script 'ad_auth' do
+    command <<-EOH.gsub(/^ (4)/, '')
+        import jenkins.model.*
+        import jenkins.security.*
+        import hudson.security.*
+        import hudson.plugins.active_directory.*
+
+        def instance = Jenkins.getInstance()
+        String domain = "#{credentials["credentials"]["ad_domain"]}"
+        String site = "#{credentials["credentials"]["ad_site"]}"
+        String server = "#{credentials["credentials"]["ad_server"]}"
+        String bindName = "#{credentials["credentials"]["ad_bindname"]}"
+        String bindPassword = "#{credentials["credentials"]["ad_bindpasswd"]}"
+        def adrealm = new ActiveDirectorySecurityRealm(domain, site, bindName, bindPassword, server, GroupLookupStrategy.RECURSIVE)
+        instance.setSecurityRealm(adrealm)
+//        def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+//        strategy.setAllowAnonymousRead(true)
+//        instance.setAuthorizationStrategy(strategy)
+        instance.save()
+    EOH
 end
 
 jenkins_command 'safe-restart'
--- chef/cookbooks/jenkins-ci/recipes/plugins.rb
+++ chef/cookbooks/jenkins-ci/recipes/plugins.rb
@@ -1,127 +1,30 @@
 #
 # Cookbook Name:: jenkins-ci
-# Recipe:: master
+# Recipe:: plugins
 #
 # Copyright 2017, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
+plugins_fname = 'plugins.list'
+plugins = File.join(Chef::Config[:file_cache_path], 'plugins.list')
 
-include_recipe "jenkins::master"
-include_recipe 'maven'
+Chef::Log.info("Uploading plugin list to #{plugins}")
 
-git_client 'default' do
-  action :install
-end
+cookbook_file plugins do
+  source 'plugins.list'
+  mode '0644'
+end.run_action(:create)
 
-git_config 'user.email' do
-    value 'jenkins@jenkins.sandbox.local'
-    scope 'global'
-    user 'jenkins'
-end
-
-git_config 'user.name' do
-    value 'Jenkins CI'
-    scope 'global'
-    user 'jenkins'
-end
-
-jenkins_plugin 'junit' do
-    version '1.2'
-    install_deps false
-end
-
-jenkins_plugin 'matrix-project' do
-    version '1.7.1'
-    install_deps false
-end
-
-jenkins_plugin 'scm-api' do
-    version '2.1.0'
-    install_deps false
-end
-
-jenkins_plugin 'credentials' do
-    version '2.1.13'
-    install_deps false
-end
-
-jenkins_plugin 'display-url-api' do
-    version '2.0'
-    install_deps false
-end
-
-jenkins_plugin 'token-macro' do
-    version '2.1'
-    install_deps false
-end
-
-jenkins_plugin 'workflow-scm-step' do
-    version '2.4'
-    install_deps false
-end
-
-jenkins_plugin 'workflow-step-api' do
-    version '2.9'
-    install_deps false
-end
-
-jenkins_plugin 'ssh-credentials' do
-    version '1.13'
-    install_deps false
-end
-
-jenkins_plugin 'plain-credentials' do
-    version '1.4'
-    install_deps false
-end
-
-jenkins_plugin 'git-client' do
-    version '2.4.5'
-    install_deps false
-end
-
-jenkins_plugin 'active-directory' do
-    install_deps false
-end
-
-jenkins_plugin 'mailer' do
-    version '1.20'
-    install_deps false
-end
-
-jenkins_plugin 'script-security' do
-    version '1.27'
-    install_deps false
-end
-
-jenkins_plugin 'structs' do
-    version '1.6'
-    install_deps false
-end
-
-jenkins_plugin 'git' do
-    version '3.3.0'
-    install_deps false
-end
-
-jenkins_plugin 'job-dsl'
-
-jenkins_script 'add_maven' do
-  command <<-EOH.gsub(/^ {4}/, '')
-    import jenkins.model.*
-    def instance = Jenkins.getInstance()
-
-    def mavenTask = Jenkins.instance.getExtensionList(
-      hudson.tasks.Maven.DescriptorImpl.class
-    )[0]
-    mavenTask.setInstallations(
-      new hudson.tasks.Maven.MavenInstallation(
-        "Maven", "/usr/local/maven-3.5.0", []
-      )
-    )
-    mavenTask.save()
-  EOH
+if File.exists?("#{plugins}")
+    File.open("#{plugins}", "r").each_line do |plugin|
+        (plugin_name, plugin_ver) = plugin.split(/:/)
+        Chef::Log.info("Jenkins plugin #{plugin_name} #{plugin_ver} will be installed")
+        jenkins_plugin "#{plugin_name}" do
+            version "#{plugin_ver}"
+            install_deps true
+        end
+    end
 end
 
 jenkins_command 'safe-restart'
