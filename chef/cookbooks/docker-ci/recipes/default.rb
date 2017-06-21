d13ca37ca98d870ded453817af6af7c300d75b67
Oleksandr Semych <Oleksandr_Semych@epam.com>
2017-06-21 10:41:59 +0300

    added docker-ci cookbook

---
+++ chef/cookbooks/docker-ci/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: docker-ci
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+docker_service 'default' do
+  host [ "tcp://0.0.0.0:4243", 'unix:///var/run/docker.sock' ]
+  action [:create, :start]
+end
+
+docker_image 'jenkinsci/jnlp-slave' do
+  action :pull
+end
+
